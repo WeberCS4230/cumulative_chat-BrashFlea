ce2f859495bdc01708d4bdeadbf6d31a9400ce7c
Jonathan Mirabile <jonathanmirabile@mail.weber.edu>
2017-09-27 22:21:42 -0600

    Continued work on Student class. Need to change underlying responses data structure to a set to provide uniqueness

--- src/ChatApplication/Student.java
+++ src/ChatApplication/Student.java
@@ -1,24 +1,38 @@
 package ChatApplication;
 
+import java.util.*;
+
 public class Student {
 	
 	protected String firstName;
 	protected String lastName;
 	protected Integer score;
-	protected String[] responses = new String[5];
+	protected List<String> responses = new ArrayList<String>();
 	
-    public Student(String fName, String lName, Integer score, 
-    		String hello, String response1, String response2, String response3, String goodbye){
+    public Student(String fName, String lName, Integer score){
     	this.firstName = fName;
     	this.lastName = lName;
-    	this.score = score;
-    	
-    	this.responses[0] = hello;
-    	this.responses[1] = response1;
-    	this.responses[2] = response2;
-    	this.responses[3] = response3;
-    	this.responses[4] = goodbye;
-    	
+    	this.score = score;   	
+    }
+    
+    public void AddResponse(String Response) {
+        this.responses.add(Response);
+    }
+    
+    public void AddResponse(String[] Response) {
+        for(int i=0; i < Response.length; i++) {
+            this.responses.add(Response[i]);
+        }
+    }
+    
+    
+    public String GetResponse(int index) {
+        //Add bounds checking
+        return this.responses.get(index);  
+    }
+    
+    public String GetResponse() {
+        return this.responses.get(responses.size() -1);
     }
 	
 }
--- src/ChatApplication/StudentChat.java
+++ src/ChatApplication/StudentChat.java
@@ -6,7 +6,13 @@ import ChatApplication.Group;
 public class StudentChat {
 
     public static void main(String[] args) {
-      Student student1 = new Student("Jonathan", "Mirabile", 1, "Heya", "How are you?", "What's going on?", "Gotta run", "Bye");
+      Student student1 = new Student("Jonathan", "Mirabile", 1);
+      String[] JonathanResponses = {"Test1", "Test2"};
+      
+      student1.AddResponse(JonathanResponses);
+      student1.AddResponse("Hello");
+      System.out.println(student1.GetResponse());
+      System.out.println(student1.GetResponse(0));
 
     }
     
