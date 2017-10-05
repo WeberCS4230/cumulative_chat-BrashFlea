b9fb49bc794b26f2f666bb17baab525d06516e94
Jonathan Mirabile <jonathanmirabile@mail.weber.edu>
2017-10-04 21:12:03 -0600

    Created chat input box
    Added a scroll bar to the graphical chat text area and added the starting compontents of the chat input box. Need to add event listeners to the input box and append the input text to the chat.

--- src/ChatApplication/GraphicalChat.java
+++ src/ChatApplication/GraphicalChat.java
@@ -13,10 +13,13 @@ public class GraphicalChat extends JFrame {
     public GraphicalChat(String textToDisplay) {
         initUI();
         showTextArea(textToDisplay);
+        showChatInput();
+        
     }
 
     private void initUI() {
         setTitle("CS3230 Graphical Chat");
+        setLayout(new GridLayout(2,1));
         setSize(400,400);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -25,10 +28,33 @@ public class GraphicalChat extends JFrame {
     
     private void showTextArea(String textToDisplay) {
         JTextArea chatArea = new JTextArea(textToDisplay, 10, 25);
+        Font hv = new Font("Helvetica", Font.PLAIN, 14);
+        chatArea.setFont(hv);
         chatArea.setLineWrap(true);
         chatArea.setWrapStyleWord(true);
         chatArea.setEditable(false);
         add(chatArea);
+        
+        JScrollPane scroll = new JScrollPane(chatArea);
+        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        add(scroll);
+        
+    }
+    
+    private void showChatInput() {
+        JTextArea chatInput = new JTextArea("Enter chat text here", 15, 10);
+        Font co = new Font("Courier", Font.ITALIC, 12);
+        chatInput.setFont(co);
+        chatInput.setLineWrap(true);
+        chatInput.setWrapStyleWord(true);
+        chatInput.setEditable(true);
+        add(chatInput);
+        
+        chatInput.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                chatInput.setText("");
+            }
+        });
         
     }
     
